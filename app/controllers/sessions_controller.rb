4e8f1ad4711c632cee07fdce5b86ee30def4fb33
ooooose <yuuki.oose1990@gmail.com>
2022-07-13 21:30:01 +0900

    Finish login logout function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email:params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_url
+      redirect_to posts_url
     else
       render 'new'
     end
